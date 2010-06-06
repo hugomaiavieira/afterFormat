e4e0e8786112e5f6be5d350538e2e5e12ab609f4
douglascamata <d.camata@gmail.com>
2010-06-06 01:06:19 -0300

    Repositório do Flashplayer 64 bits adicionado na seção "Media"

--- repositorios.sh
+++ repositorios.sh
@@ -41,6 +41,10 @@ then
     sudo wget http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list --output-document=/etc/apt/sources.list.d/medibuntu.list
     sudo apt-get update
     sudo apt-get -y --allow-unauthenticated install medibuntu-keyring
+
+    # PPA para Flashplayer 64 bits
+    sudo add-apt-repository ppa:sevenmachines/flash
+
     sudo apt-get update
 fi
 
