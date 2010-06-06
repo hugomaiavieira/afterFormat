bec8b6c6eacbd0f8e93a48fab06fe101c523b6d5
douglascamata <d.camata@gmail.com>
2010-06-06 00:57:15 -0300

    Repositório do VirtualBox adicionado.

--- repositorios.sh
+++ repositorios.sh
@@ -50,3 +50,10 @@ then
     sudo apt-get update
 fi
 
+if [ "$1" = "virtualbox" ]
+then
+    sudo echo deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) non-free > /etc/apt/sources.list.d/virtualbox.list
+    sudo apt-key add oracle_vbox.asc
+    sudo apt-get update
+fi
+
